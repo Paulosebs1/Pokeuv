7757d31e339eb0ab694e50224588273c4bee9b20
paulo <paulogonzalezc@alumnos.uv.cl>
2024-07-05 04:07:18 -0400

    v1.2

--- src/Pages/elementos.jsx
+++ src/Pages/elementos.jsx
@@ -2,9 +2,9 @@ function Elementos() {
     return <div>
         <div class="contenedor">
             <img class="logo"src="public\image.png" alt="Logo"/>
-            <h1>Elementos</h1>
+            <h1>Pokemones</h1>
         </div>
+        
     </div>
 }
-
 export default Elementos
--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -3,6 +3,13 @@ function Home() {
         <div class="contenedor">
             <img class="logo"src="public\image.png" alt="Logo"/>
         </div>
+        <div>
+            <h2 class="text-center">
+                Bienvenidos a la pokedex creada por los estudiantes de <br/> ingenieria civil informatica
+                de primer año.
+            </h2>
+            <h3 class="text-center">para crear esta web utilizamos react y bootstrap</h3>
+        </div>
     </div>
 }
 
